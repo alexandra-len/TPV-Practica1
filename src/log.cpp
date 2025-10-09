81fc28a2771a81e185b54e77217d97b55a1b4471
Zhiyi Zhou <zhiyiz01@ucm.es>
2025-10-09 12:38:09 +0200

    Log minimo implementado

--- src/log.cpp
+++ src/log.cpp
@@ -1,1 +1,18 @@
 #include "log.h"
+
+void Log::render() const
+{
+    texture->render();
+}
+
+void Log::update() 
+{
+
+}
+
+//Collision Log::checkCollision(const SDL_FRect& otherRect)
+//{
+//   
+//}
+
+
--- src/log.h
+++ src/log.h
@@ -1,5 +1,25 @@
 #pragma once
+
+#include "game.h"
+#include "texture.h"
+#include "vector2D.h"
+
 class Log
 {
+	Game* game;
+	Texture* texture;
+	Point2D<int> position;
+	Vector2D<float> speed;
+
+public:
+
+	Log(Game* g, Texture* t, Point2D<int> p, Vector2D<float> s) : game(g), texture(t), position(p), speed(s) 
+	{
+	}
+
+	void render() const;
+	void update();
+
+	Collision checkCollision(const SDL_FRect& otherRect);
 };
 
