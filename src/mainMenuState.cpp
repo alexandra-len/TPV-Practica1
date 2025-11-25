309a0c8c89d90096c5e09de30500d71c1cfcb527
Zhiyi Zhou <zhiyiz01@ucm.es>
2025-11-26 00:56:29 +0100

    mainmenu(duro)

--- src/mainMenuState.cpp
+++ src/mainMenuState.cpp
@@ -4,3 +4,68 @@
 #include "game.h"
 #include "playState.h"
 #include "gameStateMachine.h"
+#include <filesystem>
+#include <fstream>
+using namespace std;
+
+static const char* CONFIG_FILE = "config.txt";
+
+MainMenuState::MainMenuState(Game* g) : GameState(game), selectedMap()
+{
+	ifstream map(CONFIG_FILE);
+	if (map.is_open())
+		map >> selectedMap;
+
+	addObject(new Label(this, game->getTexture(Game::MENU_BACKGROUND),Point2D<int>(0, 0)));
+	//addObject(new Label(this, game->getTexture(Game::ELEGIR),Point2D<int>()));
+
+	Button* exitButton = new Button(this,game->getTexture(Game::SALIR),Point2D<int>());
+	exitButton->connect([this]() 
+		{
+			game->popState();
+		});
+	addObject(exitButton);
+}
+
+
+MainMenuState::~MainMenuState()
+{
+	ofstream map(CONFIG_FILE);
+	map << selectedMap;
+}
+
+void MainMenuState::handleEvent(const SDL_Event& event)
+{
+	GameState::handleEvent(event);
+
+    if (event.type == SDL_EVENT_KEY_DOWN)
+    {
+        switch (event.key.key)
+        {
+
+        case SDLK_LEFT:
+            if (selectedMap-1>= 0)
+            {
+                buttons[selectedMap]->setActive(false);
+
+                selectedMap--;
+
+                buttons[selectedMap]->setActive(true);
+            }
+            break;
+
+        case SDLK_RIGHT:
+            if (selectedMap+1<buttons.size())
+            {
+                buttons[selectedMap]->setActive(false);
+                selectedMap++;
+                buttons[selectedMap]->setActive(true);
+            }
+            break;
+
+        case SDLK_RETURN:
+            game->pushState();
+            break;
+        }
+    }
+}
--- src/mainMenuState.h
+++ src/mainMenuState.h
@@ -7,6 +7,11 @@ class Button;
 
 class MainMenuState : public GameState
 {
+private:
+    std::vector<std::string> maps;
+    std::vector<Button> buttons;
+    int selectedMap = 0;
+
 public:
     MainMenuState(Game* g) : GameState(g) {};
     ~MainMenuState() = default;
