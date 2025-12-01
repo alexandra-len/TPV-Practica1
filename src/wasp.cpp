2b676959a22837b324ddb7879dc2d56e9fcc0895
Alexandra <alexlent@ucm.es>
2025-12-01 21:18:20 +0100

    update

--- src/wasp.cpp
+++ src/wasp.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 Wasp::Wasp(GameState* g, Texture* t, Point2D<int> p, Vector2D<float> s, int l, int n)
-	: SceneObject(g, t, p), speed(s), lifetime(l), deathTime(SDL_GetTicks() + lifetime), lives(true), currentTime(0), nestNr(n) { }
+	: SceneObject(g, t, p), speed(s), lives(true), lifetime(l), deathTime(SDL_GetTicks() + lifetime), currentTime(0), nestNr(n) { }
 
 Wasp::Wasp(GameState* g, std::istream& input)  : SceneObject(g, g->getGame()->getTexture(Game::WASP)), lives(true), currentTime(0) {
 	int x, y, t;
